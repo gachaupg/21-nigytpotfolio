26dec1fb2b4b82f4ae783366ea2be61734b1a88d
gachaupg <gachaupg607@gmail.com>
2024-02-18 17:36:16 +0300

    ff

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,3 +1,4 @@
+
 const data = [
   { image: 'https://res.cloudinary.com/pitz/image/upload/v1708110217/3_tf61mn.png' },
   { image: 'https://res.cloudinary.com/pitz/image/upload/v1708110092/12_mt0s6f.png' },
@@ -14,16 +15,16 @@ const data = [
 
 const Clients = () => {
   return (
-    <div data-aos="fade-right" data-aos-delay="400" className="clients-container h-64 w-full flex flex-col items-center justify-center">
-      <h2 className="text-red-600 text-4xl">Our clients</h2>
+    // <div data-aos="fade-right" data-aos-delay="400" className="clients-container h-64 w-full flex flex-col items-center justify-center">
+    //   <h2 className="text-red-600 text-4xl">Our clients</h2>
       <div className="cards-wrapper gap-7">
-        {data.map((item, index) => ( // added index parameter to map function
-          <div key={index}>
-            <img className="h-80 img-abt object-contain" src={item.image} alt="" />
-          </div>
+        {data.map((item, index) => (
+          // <div className="w-full " key={index}>
+            <img key={item.image} className="h-96 w-96 object-contain" src={item.image} alt="" />
+          // </div>
         ))}
       </div>
-    </div>
+    // </div>
   );
 };
 
